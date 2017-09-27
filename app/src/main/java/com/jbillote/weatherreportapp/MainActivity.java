0661e0b5f1d3d854e70127f3ddb152343e504415
Billote, Jeremy <jeremy.billote@sap.com>
2017-09-26 17:30:58 -0700

    Add skeleton for main activity

--- app/src/main/java/com/jbillote/weatherreportapp/MainActivity.java
+++ app/src/main/java/com/jbillote/weatherreportapp/MainActivity.java
@@ -1,13 +1,58 @@
 package com.jbillote.weatherreportapp;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    // TODO: Change to ProgressBar
+    private ProgressDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void searchPressed(View view) {
+        ExecuteAPI executeAPI = new ExecuteAPI();
+        executeAPI.execute();
+    }
+
+    private class ExecuteAPI extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            dialog = new ProgressDialog(MainActivity.this);
+            dialog.setMessage(getApplicationContext().getString(R.string.searching));
+            dialog.setIndeterminate(true);
+            dialog.setCancelable(true);
+        }
+
+        @Override
+        protected Void doInBackground(Void... unused) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    dialog.show();
+                }
+            });
+
+            try {
+                Thread.sleep(10000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void unused) {
+            dialog.dismiss();
+        }
     }
 }
